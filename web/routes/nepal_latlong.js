2c2226da24161888a51062a9ce85cabda0a87576
Palista Kharel <pkharel@umass.edu>
2016-08-09 14:35:39 -0400

    Deleted '.ejs' from all from Notepad++ pages

--- web/routes/nepal_latlong.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET nepal_latlong page. */
-router.get('/nepal_latlong', function(req, res, next) {
-  res.render('nepal_latlong', { title: 'Express' });
-});
-
-module.exports = router;
--- web/routes/researchanalyses.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET researchanalyses page. */
-router.get('/researchanalyses', function(req, res, next) {
-  res.render('researchanalyses', { title: 'Express' });
-});
-
-module.exports = router;
