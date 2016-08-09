62fd289f16c2bec074f81ca945e29af4dcf8b780
Andy Anderson <aanderson@amherst.edu>
2016-08-09 15:18:07 -0400

    Update routing with simpler structure for now.

--- web/app.js
+++ web/app.js
@@ -5,13 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-// var kathmandu = require('./routes/kathmandu');
-// var springfield = require('./routes/springfield');
-// var arduinosensors = require('./routes/arduinosensors');
-var meettheteam = require('./routes/meettheteam');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -26,12 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-// app.use('/kathmandu', kathmandu);
-// app.use('/springfield', springfield);
-// app.use('/arduinosensors', arduinosensors);
-app.use('/meettheteam', meettheteam);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- web/routes/index.js
+++ web/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/arduinosensors', function(req, res, next) {
+  res.render('arduinosensors', { title: 'Express' });
+});
+
+router.get('/kathmandu', function(req, res, next) {
+  res.render('kathmandu', { title: 'Express' });
+});
+
+router.get('/springfield', function(req, res, next) {
+  res.render('springfield', { title: 'Express' });
+});
+
+router.get('/meettheteam', function(req, res, next) {
+  res.render('meettheteam', { title: 'Express' });
+});
+
+
 module.exports = router;
