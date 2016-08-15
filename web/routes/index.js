c93b47c9b667111c2ebac293978cb659fb59ed6c
Andy Anderson <aanderson@amherst.edu>
2016-08-15 17:02:53 -0400

    Added jquery to header in index.ejs and carousel.css to index.ejs in an effort to make the carousel worth

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,56 +3,55 @@ var router = express.Router();
 
 /*Header for bootstrap for every page*/
 
-var commonHeader= '<meta charset="utf-8"> \
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"> \
-    <meta name="viewport" content="width=device-width, initial-scale=1"> \
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags --> \
-    <meta name="description" content=""> \
-    <meta name="author" content=""> \
-    <link rel="icon" href="../../favicon.ico"> \
- \
-    <title>Five Colleges Open Air Quality Monitoring Project</title> \
- \
-    <!-- Bootstrap core CSS --> \
-    <link href="/css/bootstrap.min.css" rel="stylesheet"> \
-    <!-- Bootstrap theme --> \
-    <link href="/css/bootstrap-theme.min.css" rel="stylesheet"> \
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug --> \
-    <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> \
- \
-    <!-- Custom styles for this template --> \
-    <link href="theme.css" rel="stylesheet"> \
- \
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --> \
-    <!--[if lt IE 9]> \
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> \
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> \
-    <![endif]--> \
-	 \
-	<link type="text/css" rel="stylesheet" href="/css/stylesheet.css"> \
+var commonHeader= '<meta charset="utf-8"> \n\
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> \n\
+		<meta name="viewport" content="width=device-width, initial-scale=1"> \n\
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags --> \n\
+		<meta name="description" content=""> \n\
+		<meta name="author" content=""> \n\
+		<link rel="icon" href="../../favicon.ico" /> \n\
+ \n\
+		<title>Five Colleges Open Air Quality Monitoring Project</title> \n\
+ \n\
+ 	<script type="text/JavaScript" src="/js/jquery-3.1.0.slim.min.js"></script> \n\
+ 	\n\
+		<!-- Bootstrap core CSS --> \n\
+		<link href="/css/bootstrap.min.css" rel="stylesheet" /> \n\
+		<!-- Bootstrap theme --> \n\
+		<link href="/css/bootstrap-theme.min.css" rel="stylesheet" /> \n\
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug --> \n\
+		<link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet" /> \n\
+ \n\
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --> \n\
+		<!--[if lt IE 9]> \n\
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> \n\
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> \n\
+		<![endif]--> \n\
+		\n\
+		<link type="text/css" rel="stylesheet" href="/css/stylesheet.css" /> \n\
 '
 /*Header for Navigation Bar for every page*/
-var commonNavigation= '<nav class="navbar navbar-inverse navbar-fixed-top">\
-		  <div class="container">\
-			<div class="navbar-header">\
-			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">\
-				<span class="sr-only">Toggle navigation</span>\
-				<span class="icon-bar"></span>\
-				<span class="icon-bar"></span>\
-				<span class="icon-bar"></span>\
-			  </button>\
-			  <a class="navbar-brand" href="/">Open Air Quality</a>\
-			</div>\
-			<div id="navbar" class="navbar-collapse collapse">\
-			  <ul class="nav navbar-nav">\
-				<li><a href="/arduinosensors">Arduino Sensors</a></li>\
-				<li><a href="/kathmandu">Kathmandu</a></li>\
-				<li><a href="/springfield">Springfield</a></li>\
-				<li><a href="/meettheteam">Meet The Team</a></li>\
-			  </ul>\
-			</div><!--/.nav-collapse -->\
-		  </div>\
-		</nav>\
+var commonNavigation= '<nav class="navbar navbar-inverse navbar-fixed-top">\n\
+		  <div class="container">\n\
+			<div class="navbar-header">\n\
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">\n\
+				<span class="sr-only">Toggle navigation</span>\n\
+				<span class="icon-bar"></span>\n\
+				<span class="icon-bar"></span>\n\
+				<span class="icon-bar"></span>\n\
+			  </button>\n\
+			  <a class="navbar-brand" href="/">Open Air Quality</a>\n\
+			</div>\n\
+			<div id="navbar" class="navbar-collapse collapse">\n\
+			  <ul class="nav navbar-nav">\n\
+				<li><a href="/arduinosensors">Arduino Sensors</a></li>\n\
+				<li><a href="/kathmandu">Kathmandu</a></li>\n\
+				<li><a href="/springfield">Springfield</a></li>\n\
+				<li><a href="/meettheteam">Meet The Team</a></li>\n\
+			  </ul>\n\
+			</div><!--/.nav-collapse -->\n\
+		  </div>\n\
+		</nav>\n\
 '		
 
 var pages = {'/': 'index','/arduinosensors':'arduinosensors', '/kathmandu':'kathmandu', 
