b0addac807b82da99e2813e3a3e16d80e0275244
sgilman <sco.gil.777@gmail.com>
2016-08-25 16:06:51 -0400

    solved footer issue, centered graphs for video recording,

--- web/routes/index.js
+++ web/routes/index.js
@@ -61,7 +61,7 @@ var commonNavigation= '<nav class="navbar navbar-inverse navbar-fixed-top">\n\
 '		
 var commonFooter= ' <footer class="footer">\n\
       <div class="container">\n\
-        <p class="text-muted"><a href="https://github.com/kharelp/UrbanAirQuality>GitHub</a></p>\n\
+       <p class="footer-left"><a href="https://github.com/kharelp/UrbanAirQuality">GitHub</a> | GPLv3</p>\n\
       </div>\n\
     </footer>\n\
 '
